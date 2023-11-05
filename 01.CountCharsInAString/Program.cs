0c783aa88bb8f205b32c76736c520a37bee05c72
Hristov <hristovvd1@gmail.com>
2023-11-06 00:50:51 +0200

    Initial commit

---
+++ 01.CountCharsInAString/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> characters = new Dictionary<char, int>();
+
+            string word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var character = word[i];
+
+                if (character == ' ')
+                {
+                    continue;
+                }
+
+                if (!characters.ContainsKey(character))
+                {
+                    characters.Add(character, 0);
+                }
+
+                characters[character]++;
+            }
+
+            foreach (KeyValuePair<char, int> pair in characters)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
---
+++ 02.AMinerTask/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> resources = new Dictionary<string, double>();
+
+            string resource;
+            double quantity;
+
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, 0);
+                }
+
+                quantity = double.Parse(Console.ReadLine());
+                resources[resource] += quantity;
+            }
+
+            foreach (KeyValuePair<string, double> pair in resources)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
